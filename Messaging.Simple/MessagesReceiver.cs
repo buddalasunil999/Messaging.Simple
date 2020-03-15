6f1107e0057e1d7925de0f0e409899937ec20bb6
Sunil Buddala <buddalasunil999@gmail.com>
2020-03-15 15:52:05 +0530

    add logging in receiver

--- Messaging.Simple/MessagesReceiver.cs
+++ Messaging.Simple/MessagesReceiver.cs
@@ -3,20 +3,24 @@
     public class MessagesReceiver : IMessagesReceiver
     {
         private readonly Receiver receiver;
+        private readonly IMessageLogger messageLogger;
 
-        public MessagesReceiver(Receiver receiver)
+        public MessagesReceiver(Receiver receiver, IMessageLogger messageLogger)
         {
             this.receiver = receiver;
+            this.messageLogger = messageLogger;
         }
 
         public void Run()
         {
+            messageLogger.Info($"Starting {typeof(MessagesReceiver)}");
             foreach (var handler in Helper.GetAllHandlers())
             {
                 receiver.Run(new MessageConfiguration
                 {
                     Handler = handler
                 });
+                messageLogger.Info($"Receiver for {handler}");
             }
         }
     }
